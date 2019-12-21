db0935faf9775144deb7d90e23a392e64f53300b
mybios <mybios@qq.com>
2019-12-21 18:58:37 +0800

    computed_spec.lua

--- test/computed_spec.lua
+++ test/computed_spec.lua
@@ -7,14 +7,14 @@ describe(
     "computed sync",
     function()
         ---@type Computed
-        local data
+        local computedValue
         ---@type Spy
         local print
         beforeEach(
             function()
                 config.async = false
                 local v = 1
-                data =
+                computedValue =
                     vlua.computed(
                     function(self)
                         return v
@@ -33,28 +33,76 @@ describe(
             function()
                 vlua.reactiveEval(
                     function()
-                        print(data.value)
+                        print(computedValue.value)
                     end
                 )
 
                 print.allWith({{1}})
 
-                data.value = 2
-                lu.assertEquals(data.value , 2)
+                computedValue.value = 2
+                lu.assertEquals(computedValue.value , 2)
                 print.allWith({{2}})
-                data.value = 3
-                lu.assertEquals(data.value , 3)
-                data.value = 4
-                lu.assertEquals(data.value , 4)
+                computedValue.value = 3
+                lu.assertEquals(computedValue.value , 3)
+                computedValue.value = 4
+                lu.assertEquals(computedValue.value , 4)
                 print.allWith({{3}, {4}})
 
-                data.set(2)
-                lu.assertEquals(data.get() , 2)
+                computedValue.set(2)
+                lu.assertEquals(computedValue.get() , 2)
                 print.allWith({{2}})
-                data.set(3)
-                lu.assertEquals(data.get() , 3)
-                data.set(4)
-                lu.assertEquals(data.get() , 4)
+                computedValue.set(3)
+                lu.assertEquals(computedValue.get() , 3)
+                computedValue.set(4)
+                lu.assertEquals(computedValue.get() , 4)
+                print.allWith({{3}, {4}})
+            end
+        )
+        it(
+            "value",
+            function()
+                local v = 1
+                
+                local get = lu.createSpy("get")
+                local set = lu.createSpy("set")
+                computedValue = vlua.computed(
+                    function(self)
+                        get(self, v)
+                        return v
+                    end,
+                    function(self, newValue)
+                        set(self, newValue, v)
+                        v = newValue
+                    end
+                )
+
+                local data = vlua.reactive({id = computedValue})
+
+                vlua.reactiveEval(
+                    function()
+                        print(data.id)
+                    end
+                )
+
+                print.allWith({{1}})
+
+                data.id = 2
+                lu.assertEquals(data.id , 2)
+                print.allWith({{2}})
+
+                data.id = 3
+                lu.assertEquals(data.id , 3)
+                data.id = 4
+                lu.assertEquals(data.id , 4)
+                print.allWith({{3}, {4}})
+
+                computedValue.set(2)
+                lu.assertEquals(computedValue.get() , 2)
+                print.allWith({{2}})
+                computedValue.set(3)
+                lu.assertEquals(computedValue.get() , 3)
+                computedValue.set(4)
+                lu.assertEquals(computedValue.get() , 4)
                 print.allWith({{3}, {4}})
             end
         )
--- vlua.lua
+++ vlua.lua
@@ -2,10 +2,14 @@ local Computed = require("observer.Computed")
 local Observer = require("observer.Observer")
 local Ref = require("observer.Ref")
 local ReactiveEval = require("observer.ReactiveEval")
-
+local observe = Observer.observe
+local function reactive(value)
+    observe(value)
+    return value
+end
 return {
     ref = Ref.ref,
     computed = Computed.computed,
-    reactive = Observer.observe,
+    reactive = reactive,
     reactiveEval = ReactiveEval.reactiveEval
 }
